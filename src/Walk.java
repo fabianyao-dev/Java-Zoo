f406360682483faaad74cee70968af4b645f00b6
Fabian Ramirez <fabianyao02@hotmail.com>
2026-05-19 05:49:18 +0000

    Redundancia eliminada

--- src/Walk.java
+++ src/Walk.java
@@ -1,6 +1,6 @@
-public interface Walk {
+ interface Walk {
     /** TODO 4: declare a functionality or method named
      *          "walking" with return type "void"
      */
-    public void walking();
+     void walking();
 }
